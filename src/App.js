db787fa6d94592245a8078c6f5d80e1b82cc2354
Arne Rief <arne-rief_student2022@wilder.school>
2022-04-19 18:25:55 +0200

    working template without routes, central user input and buttons to display different API results for weather & hotels

--- src/App.js
+++ src/App.js
@@ -2,32 +2,60 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  // states for our data:
+  // states for user destination and all the API data:
+  let [userDestination, setUserDestination] = useState("");
   let [cityData, setCityData] = useState({});
   let [weatherData, setWeatherData] = useState({});
-  let [visaData, setVisaData] = useState({});
-  let [userDestination, setUserDestination] = useState("");
+  let [hotelData, setHotelData] = useState({});
+ 
+  // states for handling the display of different sections & for displaying API results
+  let [currentSection, setCurrentSection] = useState("main");
+  let [apiLoaded, setApiLoaded] = useState(false);
 
   // Function updating the state variable userDestination with the text the user types
   const handleInput = function(event) {
     setUserDestination(event.currentTarget.value)
   }
 
-  // Function to display results of API request when the button was clicked:
-  let [apiLoaded, setApiLoaded] = useState(false);
+  // Function to switch which section is displayed on button click
+  const switchDisplay = function(section) {
+    setCurrentSection(section);
+  }
+
+  // Function receiving geo-coordinates as argument to querying hotels API 
+  const getHotels = function(input) {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Host': 'booking-com.p.rapidapi.com',
+        'X-RapidAPI-Key': process.env.REACT_APP_HOTELKEY
+      }
+    }
+
+    fetch(`https://booking-com.p.rapidapi.com/v1/hotels/search-by-coordinates?longitude=${input[0].lon}&latitude=${input[0].lat}&checkin_date=2022-09-30&locale=en-gb&filter_by_currency=AED&checkout_date=2022-10-01&room_number=1&units=metric&adults_number=2&order_by=popularity&include_adjacency=true&page_number=0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0&children_number=2`, options)
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data)
+        // console.log(data.result[0].hotel_name)
+        setHotelData(data.result);
+      })
+  }
   
   // Function to get API results for the city from the text input, on button click:
-  const getCityCoordinates = function(input) {
+  const getCityInfo = function(input) {
     // first get geo-coordinates from Geocoding API according to user input
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=${process.env.REACT_APP_WEATHERKEY}`)
     .then((response) => response.json())
     .then((data) => {
       setCityData(data);
-      // second get weather for today and next 7 days from OpenWeather API
+      // second, use geo-coordinates from data to search hotel API with getHotels function
+      getHotels(data);
+      // third, get weather for today and next 7 days from OpenWeather API with the coordinates
       fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data[0].lat}&lon=${data[0].lon}&exclude=minutely,hourly&appid=${process.env.REACT_APP_WEATHERKEY}&units=metric`)
       .then((response) => response.json())
       .then((data2) => {
           setWeatherData(data2);
+          // update state for API display 
           setApiLoaded(true);
       });
     });
@@ -35,6 +63,7 @@ function App() {
     setUserDestination("");
   }
 
+  // Function to convert unix timestamps into human-readable dates
   function timeConverter(UNIX_timestamp) {
     var a = new Date(UNIX_timestamp * 1000);
     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
@@ -53,33 +82,92 @@ function App() {
   return (
     <div className="App">
       <h1>Sandbox API Test</h1>
-      <h3>Give us a city name and click this button to get the city geo-coordinates</h3>
-      {/* Input updates userDestination state every time the user types something */}
-      <input type="text" value={userDestination} onChange={handleInput}></input>
-      {/* Button click sends userDestination as argument to function getCityCoordinates for API call */}
-      <button onClick={() => getCityCoordinates(userDestination)}>Go!</button>
+      {currentSection === "main" &&
+      <>
+        <button className='section-btn' onClick={() => switchDisplay("weather")}>Weather</button>
+        <button className='section-btn' onClick={() => switchDisplay("flights")}>Flights</button>
+        <button className='section-btn' onClick={() => switchDisplay("hotels")}>Hotels</button>
+        <h3>Give us a city name and click this button to get the city geo-coordinates</h3>
+        {/* Input updates userDestination state every time the user types something */}
+        <input type="text" value={userDestination} onChange={handleInput}></input>
+        {/* Button click sends userDestination as argument to function getCityInfo for API call */}
+        <button onClick={() => getCityInfo(userDestination)}>Go!</button>
+        {/* Displaying API results only if user searched at least once */}
+        {apiLoaded == true &&
+        <>
+          <h3>You searched for: {cityData[0].name}</h3>
+          <p>Please check the according sections to see the weather, suitable flights and the best hotels for your travel destination</p>
+        </>
+        }
+      </>
+    }
 
-      {/* Displaying API results only if user searched at least once */}
+    {currentSection === "weather" && 
+     <>
+      <button className='section-btn' onClick={() => switchDisplay("main")}>New Search</button>
+      <button className='section-btn' onClick={() => switchDisplay("flights")}>Flights</button>
+      <button className='section-btn' onClick={() => switchDisplay("hotels")}>Hotels</button>
+
       {apiLoaded === true 
-        ? <>
-            <p><b>City:</b> {cityData[0].name} | <b>Lat:</b> {cityData[0].lat} | <b>Long:</b> {cityData[0].lon}</p>
-            <p><b>Sky:</b> {weatherData.current.weather[0].main}, {weatherData.current.weather[0].description} | <b>Temperature:</b> {weatherData.current.temp}°</p>
-            <img src={`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}.png`}
-            alt={weatherData.current.weather[0].description}/>
-            {/* Mapping over array with weather forecast */}
-            <h1>Forecast</h1>
-            {weatherData.daily.map((element, index) => 
+          ? <>
+              <p><b>City:</b> {cityData[0].name} | <b>Lat:</b> {cityData[0].lat} | <b>Long:</b> {cityData[0].lon}</p>
+              <h3>Current Weather:</h3>
+              <p><b>Sky:</b> {weatherData.current.weather[0].main}, {weatherData.current.weather[0].description} | <b>Temperature:</b> {weatherData.current.temp}°</p>
+              <img src={`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}.png`}
+              alt={weatherData.current.weather[0].description}/>
+              {/* Mapping over array with weather forecast */}
+              <h1>Forecast</h1>
+              {weatherData.daily.map((element, index) => 
+              <div key={index}>
+              <p>Date: {timeConverter(element.dt)}</p>
+              <p>Temp: {element.temp.day}° | Min: {element.temp.min}° | Max: {element.temp.max}°</p>
+              <p>Sky: {element.weather[0].main}, {element.weather[0].description}</p>
+              <img src={`http://openweathermap.org/img/wn/${element.weather[0].icon}.png`} />
+              </div>
+              )}
+            </>
+          : <p>You did not search for anything yet.</p>
+        }
+     </>
+    }
+
+  {currentSection === "flights" && 
+     <>
+      <button className='section-btn' onClick={() => switchDisplay("main")}>New Search</button>
+      <button className='section-btn' onClick={() => switchDisplay("weather")}>Weather</button>
+      <button className='section-btn' onClick={() => switchDisplay("hotels")}>Hotels</button>
+    </>
+  }
+
+  {currentSection === "hotels" && 
+     <>
+      <button className='section-btn' onClick={() => switchDisplay("main")}>New Search</button>
+      <button className='section-btn' onClick={() => switchDisplay("weather")}>Weather</button>
+      <button className='section-btn' onClick={() => switchDisplay("flights")}>Flights</button>
+
+      {apiLoaded === true
+        ? hotelData.map((element, index) => {
+          return (
             <div key={index}>
-            <p>Date: {timeConverter(element.dt)}</p>
-            <p>Temp: {element.temp.day}° | Min: {element.temp.min}° | Max: {element.temp.max}°</p>
-            <p>Sky: {element.weather[0].main}, {element.weather[0].description}</p>
-            <img src={`http://openweathermap.org/img/wn/${element.weather[0].icon}.png`} />
+              <p><b>Hotel name:</b> <i>{element.hotel_name} </i>
+              </p>
+              <p><b>Hotel address:</b> <i>{element.address}</i>
+              </p>
+              <p>
+              <b>Review:</b> <i>Score: {element.review_score}, Nr of reviews: {element.review_nr}</i></p>
+              <p>
+              <b>Link to book hotel:</b> <i>{element.url}</i></p>
+              <p>
+              <b>Best price:</b> <i>{element.price_breakdown.gross_price}, {element.price_breakdown.currency}</i></p>
+              <hr />
             </div>
-            )}
-          </>
+          )
+          })
         : <p>You did not search for anything yet.</p>
       }
-    </div>
+      </>
+  }
+  </div>
   );
 }
 
